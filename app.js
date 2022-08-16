1db4bf9a26be1ae404f0c3b47188b15619912549
vikingviolinist <misopetrik@gmail.com>
2022-08-16 10:54:44 +0000

    🎨 Format app

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Lorem ipsum');
+	res.send('Lorem ipsum');
 })
+
+app.get('/demo', (req, res) => {
+	res.set('X-full-stack', 'test');
+	res.status(418);
+	res.send('I\'m a teapot...');
+})
+
 
 app.listen(port, () => console.log(`Server is up and runnig on port ${port}`))
 
